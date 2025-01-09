dd0a1fce6ec71225d2e6edba90a21ef9483e10dc
Prapti Acharya <praptiacharya@Praptis-MacBook-Air.local>
2025-01-09 21:57:22 +0545

    Project setup completed and some frontend done

---
+++ Yoyo-Hotels/database/migrations/2025_01_09_152055_add_total_price_field_to_bookings.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Nette\Utils\Strings;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->string('total_price')->nullable()->after('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+           $table->dropColumn('total_price');
+        });
+    }
+};
---
+++ Yoyo-Hotels/resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+  <img style="height: 45px;" src="{{asset('/img/hotel-log.png')}}" alt="">
+</a>
